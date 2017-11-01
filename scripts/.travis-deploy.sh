bd9f05b210b07cfb435bec1988f2e0560c0a67fe
bdistin <bdistin@gmail.com>
2017-10-31 22:10:11 -0500

    think I got travis now...

--- scripts/.travis-deploy.sh
+++ scripts/.travis-deploy.sh
@@ -14,7 +14,7 @@ git clone $REPO dist -b $TARGET_BRANCH
 NEW_PACKAGE_VERSION=$(node -p "require('./package.json').version")
 OLD_PACKAGE_VERSION=$(node -p "require('./dist/package.json').version")
 
-if [ "$TRAVIS_PULL_REQUEST" == "true" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "stable pr - checking semvar without publishing."
   node ./scripts/versionCompare.js $NEW_PACKAGE_VERSION $OLD_PACKAGE_VERSION
   exit 0
