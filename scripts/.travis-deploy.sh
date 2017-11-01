3646e5b8e1eb5d63782766bc89e13b641be2c3a6
bdistin <bdistin@gmail.com>
2017-10-31 22:52:21 -0500

    oops

--- scripts/.travis-deploy.sh
+++ scripts/.travis-deploy.sh
@@ -8,20 +8,18 @@ if [ "$TRAVIS_BRANCH" != "stable" -o -n "$TRAVIS_TAG" ]; then
   exit 0
 fi
 
-REPO=$(git config remote.origin.url)
-TARGET_BRANCH="stable"
-git clone $REPO dist -b $TARGET_BRANCH
-NEW_PACKAGE_VERSION=$(node -p "require('./package.json').version")
-OLD_PACKAGE_VERSION=$(node -p "require('./dist/package.json').version")
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  REPO=$(git config remote.origin.url)
+  TARGET_BRANCH="stable"
+  git clone $REPO dist -b $TARGET_BRANCH
+  NEW_PACKAGE_VERSION=$(node -p "require('./package.json').version")
+  OLD_PACKAGE_VERSION=$(node -p "require('./dist/package.json').version")
   echo -e "stable pr - checking semvar without publishing."
   node ./scripts/versionCompare.js $NEW_PACKAGE_VERSION $OLD_PACKAGE_VERSION
   exit 0
 fi
 
 echo -e "Publishing for a stable branch push."
-
-node ./scripts/versionCompare.js $NEW_PACKAGE_VERSION $OLD_PACKAGE_VERSION
 
 npx vsce publish -p "${PUBLISH_TOKEN}"
