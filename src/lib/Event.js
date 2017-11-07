134e444ce175fdda84e1e5c1a67d24e186932166
Pandraghon <ryuunushi@gmail.com>
2017-11-07 11:00:28 +0100

    Add onDidChangeTextDocument.js

--- src/lib/Event.js
+++ src/lib/Event.js
@@ -10,11 +10,11 @@ class Event {
 		this.container = options.container;
 	}
 
-	_run() {
+	_run(...args) {
 		// common checks
 		if (!workspace.rootPath) return undefined;
 
-		return this.run().catch(err => {
+		return this.run(...args).catch(err => {
 			if (err === undefined) return false;
 			if (typeof err === 'string') return window.showErrorMessage(err);
 			return console.error(err);
