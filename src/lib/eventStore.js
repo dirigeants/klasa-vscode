52386af6993832f1e4ceb2c4488abe942e74b88e
AoDude <bdistin@gmail.com>
2018-03-28 11:19:46 -0500

    update events for v0.5.0

--- src/lib/eventStore.js
+++ src/lib/eventStore.js
@@ -5,24 +5,51 @@ class EventStore extends Array {
 		return this;
 	}
 
+	sort() {
+		return super.sort((a, b) => {
+			const nameA = a.label.toUpperCase();
+			const nameB = b.label.toUpperCase();
+			if (nameA < nameB) return -1;
+			if (nameA > nameB) return 1;
+			return 0;
+		});
+	}
+
 }
 
 module.exports = new EventStore()
+	// Klasa Events
+	.addEvent('commandError', 'message, command, params, error', 'Emitted when a command has errored.')
+	.addEvent('commandInhibited', 'message, command, response', 'Emitted when a command has been inhibited.')
+	.addEvent('commandRun', 'message, command, args', 'Emitted when a command is about to be run.')
+	.addEvent('commandUnknown', 'message, command', 'Emitted when an unknown command is called.')
+	.addEvent('commandSuccess', 'message, command, params, response', 'Emitted when a command has successfully run.')
+	.addEvent('configCreateEntry', 'entry', 'Emitted when config entries are created.')
+	.addEvent('configDeleteEntry', 'entry', 'Emitted when config entries are deleted.')
+	.addEvent('configUpdateEntry', 'oldEntry, newEntry, path', 'Emitted when config entries are updated or reset.')
+	.addEvent('eventError', 'event, args, error', 'Emitted when an event has errored.')
+	.addEvent('finalizerError', 'message, response, timer, finalizer, error', 'Emitted when a finalizer has errored.')
+	.addEvent('monitorError', 'message, monitor, error', 'Emitted when a monitor has errored.')
+	.addEvent('taskError', 'scheduledTask, task, error', 'Emitted when a task has errored.')
+	.addEvent('pieceLoaded', 'piece', 'Emitted when a piece is loaded.')
+	.addEvent('pieceUnloaded', 'piece', 'Emitted when a piece is unloaded.')
+	.addEvent('pieceReloaded', 'piece', 'Emitted when a piece is reloaded.')
+	.addEvent('pieceEnabled', 'piece', 'Emitted when a piece is enabled.')
+	.addEvent('pieceDisabled', 'piece', 'Emitted when a piece is disabled.')
+	.addEvent('schemaKeyAdd', 'key', 'Emitted when a key has been added to the schema.')
+	.addEvent('schemaKeyRemove', 'key', 'Emitted when a key has been removed from the schema.')
+	.addEvent('schemaKeyUpdate', 'key', 'Emitted when a key has been updated in the schema.')
+	.addEvent('klasaReady', null, 'Emitted when klasa is fully ready and initialized.')
+	.addEvent('log', 'data, type', 'A central logging event for klasa.')
+	.addEvent('verbose', 'data', 'An event for handling verbose logs')
+	.addEvent('wtf', 'data', 'An event for handling wtf logs (what a terrible failure)')
+	// D.JS Events
 	.addEvent('channelCreate', 'channel', 'Emitted whenever a channel is created.')
 	.addEvent('channelDelete', 'channel', 'Emitted whenever a channel is deleted.')
 	.addEvent('channelPinsUpdate', 'channel, time', 'Emitted whenever the pins of a channel are updated.')
 	.addEvent('channelUpdate', 'oldChannel, newChannel', 'Emitted whenever a channel is updated - e.g. name change, topic change.')
 	.addEvent('clientUserGuildSettingsUpdate', 'clientUserGuildSettings', "Emitted whenever the client user's settings update.")
 	.addEvent('clientUserSettingsUpdate', 'clientUserSettings', "Emitted whenever the client user's settings update.")
-	.addEvent('commandError', 'message, command, params, error', 'Emitted when a command has errored.')
-	.addEvent('commandInhibited', 'message, command, response', 'Emitted when a command has been inhibited.')
-	.addEvent('commandRun', 'message, command, params, response', 'Emitted when a command has been run.')
-	.addEvent('commandUnknown', 'message, command', 'Emitted when an unknown command is called.')
-	.addEvent('pieceLoaded', 'piece', 'Emitted when a piece is loaded.')
-	.addEvent('pieceUnloaded', 'piece', 'Emitted when a piece is unloaded.')
-	.addEvent('pieceReloaded', 'piece', 'Emitted when a piece is reloaded.')
-	.addEvent('pieceEnabled', 'piece', 'Emitted when a piece is enabled.')
-	.addEvent('pieceDisabled', 'piece', 'Emitted when a piece is disabled.')
 	.addEvent('debug', 'info', 'Emitted for general debugging information.')
 	.addEvent('disconnect', 'event', "Emitted when the client's WebSocket disconnects and will no longer attempt to reconnect.")
 	.addEvent('emojiCreate', 'emoji', 'Emitted whenever a custom emoji is created in a guild.')
@@ -41,8 +68,6 @@ module.exports = new EventStore()
 	.addEvent('guildMemberUpdate', 'oldMember, newMember', 'Emitted whenever a guild member changes - i.e. new role, removed role, nickname.')
 	.addEvent('guildUnavailable', 'guild', 'Emitted whenever a guild becomes unavailable, likely due to a server outage.')
 	.addEvent('guildUpdate', 'oldGuild, newGuild', 'Emitted whenever a guild is updated - e.g. name change.')
-	.addEvent('klasaReady', null, 'Emitted when klasa is fully ready and initialized.')
-	.addEvent('log', 'data, type', 'A central logging event for klasa.')
 	.addEvent('message', 'message', 'Emitted whenever a message is created.')
 	.addEvent('messageDelete', 'message', 'Emitted whenever a message is deleted.')
 	.addEvent('messageDeleteBulk', 'messages', 'Emitted whenever messages are deleted in bulk.')
@@ -57,12 +82,10 @@ module.exports = new EventStore()
 	.addEvent('roleCreate', 'role', 'Emitted whenever a role is created.')
 	.addEvent('roleDelete', 'role', 'Emitted whenever a guild role is deleted.')
 	.addEvent('roleUpdate', 'oldRole, newRole', 'Emitted whenever a guild role is updated.')
-	.addEvent('settingUpdate', 'gateway, id, oldEntries, newEntries', 'Emitted when SettingGateway.update, SettingGateway.updateArray or SettingGateway.reset is run.')
 	.addEvent('typingStart', 'channel, user', 'Emitted whenever a user starts typing in a channel.')
 	.addEvent('typingStop', 'channel, user', 'Emitted whenever a user stops typing in a channel.')
 	.addEvent('userNoteUpdate', 'user, oldNote, newNote', 'Emitted whenever a note is updated.')
 	.addEvent('userUpdate', 'oldUser, newUser', "Emitted whenever a user's details (e.g. username) are changed.")
-	.addEvent('verbose', 'data', 'An event for handling verbose logs')
 	.addEvent('voiceStateUpdate', 'oldMember, newMember', 'Emitted whenever a user changes voice state - e.g. joins/leaves a channel, mutes/unmutes.')
 	.addEvent('warn', 'info', 'Emitted for general warnings.')
-	.addEvent('wtf', 'data', 'An event for handling wtf logs (what a terrible failure)');
+	.sort();
